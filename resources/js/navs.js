5a04be017e35ded191466c775773fb05366c6b50
MigueValiente <miguevaliente96@gmail.com>
2019-02-11 18:13:13 +0100

    Terminado Ajax navs

--- resources/js/navs.js
+++ resources/js/navs.js
@@ -1,7 +1,7 @@
 $(function(){
     $('#myTabPill a').on('show.bs.tab', function (e) {
-      axios.get(`/obtenerVista/${e.target.id}`
-      ).then(function(response){
+      axios.get(`/obtenerVista/${e.target.id}`)
+      .then(function(response){
             $(`#div-${e.target.id}`).html(response.data);
             console.log(response.data);
         }).catch(function(error){
